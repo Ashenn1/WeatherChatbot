baa2542015a454814c514288af2cbd944092ae33
Ashenn1 <sohasanad@gmail.com>
2018-06-08 12:45:22 +0200

    My commits

--- weatherBot/wsgi.py
+++ weatherBot/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+project_home = u'C:\Users\Soha Samad\Desktop\College\year 3\Second Semester\SWE for PC-Human Interaction\ChatBotCode\Bot\weatherBot'
+
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weatherBot.settings")
 
