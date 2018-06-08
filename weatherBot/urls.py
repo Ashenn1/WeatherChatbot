fa3f098139b157e254c768d34b70406e99d0b4ed
Ashenn1 <sohasanad@gmail.com>
2018-06-08 12:33:16 +0200

    My Commits

--- weatherBot/urls.py
+++ weatherBot/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^fb_weatherBot/', include('fb_weatherBot.urls')),
